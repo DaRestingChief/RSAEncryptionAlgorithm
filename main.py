6be747c05058c1b540a76c764d00e8dc803f39ae
DaRestingChief <aakarshsinha06@gmail.com>
2025-11-02 03:22:58 +0530

    Code Down. Readme left

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
 import random
 import math
+
+def is_prime(number):
+    if number<2:
+        return False
+    for i in range(2, number // 2+1):
+        if number % i == 0:
+            return False
+        
+    return True
+
+def generate_prime(min_value, max_value):
+    prime= random.randint(min_value, max_value)
+    while not is_prime(prime):
+        prime= random.randint(min_value, max_value)
+
+    return prime 
+
+def mod_inverse(e, phi):
+    for d in range(3, phi):
+        if (d * e) % phi == 1:
+            return d
+    raise ValueError("Modular inverse does not exist")
+
+p,q= generate_prime(1000, 5000), generate_prime(1000, 5000)
+
+while p==q:
+    q= generate_prime(1000, 5000)
+
+n= p * q
+
+phi_n= (p - 1) * (q - 1) # Euler's totient function
+
+e=random.randint(3, phi_n-1) #Public Key (Exponent)
+
+while math.gcd(e, phi_n) != 1:
+    e=random.randint(3, phi_n-1)
+
+d= mod_inverse(e, phi_n) #Private Key (Exponent)
+
+print("Public Key: ",e)
+print("Private Key: ",d)
+print("n: ",n)
+print("Phi of n:", phi_n)
+print("p: ",p)
+print("q: ",q)
+
+message= input("Enter message to be encrypted: ")
+
+message_encoded= [ord(ch) for ch in message]
+#(m^e) mod n= c
+ciphertext= [pow(ch, e, n) for ch in message_encoded]
+
+print(ciphertext)
+
+message_encoded= [pow(ch, d, n) for ch in ciphertext]
+
+message= "".join([chr(ch) for ch in message_encoded])
+
+print (message)
